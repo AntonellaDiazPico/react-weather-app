ed8254f3b308825f39676b073befad905ff18081
AntonellaDiazPico <Anto@192-168-1-103.tpgi.com.au>
2021-04-28 16:28:01 +1000

    Added SearchForm integration & added loader spinner

--- src/WeatherEngine.js
+++ src/WeatherEngine.js
@@ -1,17 +1,15 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
-
 import CityWeatherInfo from "./CityWeatherInfo.js";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
-import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
-import { faCloudShowersHeavy } from '@fortawesome/free-solid-svg-icons'
-
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
+import { faCloudShowersHeavy } from "@fortawesome/free-solid-svg-icons";
+import Loader from "react-loader-spinner";
 
 export default function WeatherEngine(props) {
   const [ready, setReady] = useState(false);
   const [weatherData, setWeatherData] = useState({});
+  const [city, setCity] = useState(props.defaultCity);
 
   function handleCityResponse(response) {
     setWeatherData({
@@ -24,43 +22,56 @@ export default function WeatherEngine(props) {
       wind: Math.round(response.data.wind.speed),
       icon: <FontAwesomeIcon icon={faCloudShowersHeavy} />,
     });
-  setReady(true);  
+    setReady(true);
+  }
+
+  function searchGenerator() {
+    let apiKey = "7397d5769aa7c8ab77c0945b1e990b7d";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleCityResponse);
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    searchGenerator();
   }
 
   if (ready) {
-    return(
+    return (
       <div className="WeatherEngine">
-      {/* SEARCH ENGINE BEGINS */}
-    <div className="SearchEngine">
-      <div className="row">
-        <div className="col d-flex justify-content-center">
-          <form className="searchBar">
-            <input
-              className="citySearch"
-              type="text"
-              placeholder="Where to today?"
-              autoComplete="off"
-            />
-          </form>
-          <button className="location-btn">
-          <FontAwesomeIcon icon={faMapMarkerAlt} />
-          </button>
+        {/* SEARCH ENGINE BEGINS */}
+        <div className="SearchEngine">
+          <div className="row">
+            <div className="col d-flex justify-content-center">
+              <form className="searchBar" onSubmit={handleFormSubmit}>
+                <input
+                  className="citySearch"
+                  type="text"
+                  placeholder="Where to today?"
+                  autoComplete="off"
+                  onChange={handleCityChange}
+                />
+              </form>
+              <button className="location-btn">
+                <FontAwesomeIcon icon={faMapMarkerAlt} />
+              </button>
+            </div>
+          </div>
         </div>
+        {/* SEARCH ENGINE ENDS */}
+        <CityWeatherInfo cityData={weatherData} />
       </div>
-    </div>
-    {/* SEARCH ENGINE ENDS */}
-    <CityWeatherInfo cityData={weatherData} />
-
-    </div>
     );
-    
   } else {
-    let apiKey="7397d5769aa7c8ab77c0945b1e990b7d";
-    let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleCityResponse);
-
-   return "Loading"
+    searchGenerator();
+    return (
+      <div className="loader">
+        <Loader type="ThreeDots" color="rgb(250, 240, 108)" height={100} width={100} />
+      </div>
+    );
   }
-  
 }
